2a910ba7e14226f301e2c8442a54969663f05810
Edmund Home <edmundhome@hotmail.com>
2019-09-30 14:18:24 +0200

    menu en proceso

--- JavaMoviesProjectG2/src/gui/Menu.java
+++ JavaMoviesProjectG2/src/gui/Menu.java
@@ -13,15 +13,19 @@ public class Menu {
 		
 		Writer.escribirPantalla("           **********************");
 		Writer.escribirPantalla("           ***** MOVIE-FLIX *****");
-		Writer.escribirPantalla("           **********************");
-		Writer.escribirPantalla("\n");
-		Writer.escribirPantalla("***** Elige una de las siguientes opciones: *****");
-		Writer.escribirPantalla("***** 1. Crear usuario \n ***** 2. Modificar usuario **** \n");
+		Writer.escribirPantalla("           **********************"
+				+ "\n");
+
+		Writer.escribirPantalla("***** Elige una de las siguientes opciones:\n"
+				+ "           ***** 1. Crear usuario\n"
+				+ "           ***** 2. Modificar usuario\n"
+				+ "           ***** 3. Eliminar usuario\n");
+		Writer.escribirPantalla("");
 		Writer.escribirPantalla("*");
 		Writer.escribirPantalla("***** 3.Eliminar usuario");
 		Writer.escribirPantalla("*****");
 		LecturaDato.LeerInt("Elige una de las siguientes opciones\n"
-				+ "1--")
+				+ "1--");
 		
 		return 0;//Modificar
 	}
