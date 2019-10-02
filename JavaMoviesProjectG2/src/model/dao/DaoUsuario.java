5fc4a42ff0785997260db4101fde59e2aeb3a680
Edmund Home <edmundhome@hotmail.com>
2019-10-02 10:59:58 +0200

    añadido documento a DaoUsuario

--- JavaMoviesProjectG2/src/model/dao/DaoUsuario.java
+++ JavaMoviesProjectG2/src/model/dao/DaoUsuario.java
@@ -1,5 +1,11 @@
+
 /**
- * @author Julian Bautista Velez
+ * Interface DaoUsuario
+ *
+ * Contiene coleccion de metodos abstractos
+ *
+ * @author  Julian Bautista Velez
+ * 
  */
 package model.dao;
 
@@ -8,12 +14,37 @@ import java.util.ArrayList;
 import model.Usuario;
 
 public interface DaoUsuario {
-
-
+	/**
+	* Metodo para insertar Usuario
+	* 
+	* @param user
+	* 
+	*/
 	public void insertaUsuario(Usuario user);
+	/**
+	* Metodo para eliminar usuario
+	* 
+	* @param id
+	* 
+	*/
 	public void eliminaUsuario(int id);
+	/**
+	* Metodo para buscar un usuario por ID
+	* Devuelve un objeto tipo usuario
+	* @param id
+	* 
+	*/
 	public Usuario buscarID(int id);
+	/**
+	* Metodo para actualizar usuario
+	* @param user
+	* 
+	*/
 	public void actualizaUsuario(Usuario user);
+	/**
+	* Metodo para listar Usuarios
+	* Devuelve una lista de Usuarios de tipo ArrayList
+	*/
 	public ArrayList<Usuario> listarUsuario() throws Exception;
 	
 }
